88082b5a58f87139cf50dd3647fd62703032a7a5
ParimalKesan <parimalkesan3@gmail.com>
2020-07-17 01:45:13 +0530

    added covid update layouts

--- app/src/main/java/com/example/jeewan/CovidUpdates.java
+++ app/src/main/java/com/example/jeewan/CovidUpdates.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class get_started extends AppCompatActivity {
+public class CovidUpdates extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_started);
+        setContentView(R.layout.activity_covid_updates);
     }
 }
--- app/src/main/java/com/example/jeewan/SplashActivity.java
+++ app/src/main/java/com/example/jeewan/SplashActivity.java
@@ -5,20 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, get_started.class);
+                Intent intent = new Intent(SplashActivity.this, get_started.class);
                 startActivity(intent);
             }
         }, 2500);
