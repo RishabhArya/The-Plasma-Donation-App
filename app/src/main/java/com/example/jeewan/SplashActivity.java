e533d2bb21d15f814ed6b41e44c8efaabf58f96d
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-17 12:02:09 +0530

    Refractored the UI

--- app/src/main/java/com/example/jeewan/SplashActivity.java
+++ app/src/main/java/com/example/jeewan/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, get_started.class);
+                Intent intent = new Intent(SplashActivity.this, CovidUpdates .class);
                 startActivity(intent);
             }
         }, 2500);
