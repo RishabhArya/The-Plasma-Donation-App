b91de4f72776b8609d0f2f6b700d7a80753dc36b
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-19 20:43:57 +0530

    Profile

---
+++ app/src/main/java/com/example/jeewan/Profile/ProfileModel.java
@@ -0,0 +1,18 @@
+package com.example.jeewan.Profile;
+
+public class ProfileModel {
+    String name;
+    String age;
+    String contact;
+    String city;
+    String state;
+
+
+    public ProfileModel(String name, String age, String contact, String city, String state) {
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+        this.city = city;
+        this.state = state;
+    }
+}
