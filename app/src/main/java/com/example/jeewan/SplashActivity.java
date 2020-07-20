7d4e5e808047884b6e3088bb5d8dfb542e29c371
Parimal Kesan <parimalkesan3@gmail.com>
2020-07-20 13:31:47 +0530

    Update SplashActivity.java

--- app/src/main/java/com/example/jeewan/SplashActivity.java
+++ app/src/main/java/com/example/jeewan/SplashActivity.java
@@ -3,10 +3,13 @@ package com.example.jeewan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import com.example.jeewan.authentication.Mainlogin;
+import com.example.jeewan.onboarding.screens.OnBoarding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -26,17 +29,31 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (currentUser != null) {
-                    //move to MainScreen activity
-                    Intent intent = new Intent(SplashActivity.this, MainScreenActivity.class);
-                    startActivity(intent);
+                onBoardingScreen = getSharedPreferences("OnBoardingScreen",MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firsttime",true);
+                if (currentUser == null) {
+                    if(isFirstTime)
+                    {
+                        //move to OnBoarding activity
+                         SharedPreferences.Editor editor = onBoardingScreen.edit();
+                         editor.putBoolean("firsttime",false);
+                         editor.commit();
+                         Intent intent = new Intent(SplashActivity.this, OnBoarding.class);
+                         startActivity(intent);
+                    }
+                    else
+                    {
+                        //move to activity_mainlogin activity
+                        Intent intent = new Intent(SplashActivity.this, Mainlogin.class);
+                        startActivity(intent);
+                    }
                 }
-                else {
-                    //move to activity_mainlogin activity
-                    Intent intent = new Intent(SplashActivity.this, Mainlogin.class);
-                    startActivity(intent);
-                }
-            }
+                else
+                {
+                    //move to MainScreen Activity
+                     Intent intent = new Intent(SplashActivity.this, MainScreenActivity.class);
+                     startActivity(intent);
+                }           
         }, 2500);
     }
 
