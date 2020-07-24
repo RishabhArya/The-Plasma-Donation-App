307ef0a836fadb63b59ce129fcb5664880591938
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-07-24 13:05:06 +0530

    Merge Confflic

--- app/src/main/java/com/example/jeewan/menuIeams/send_feedback.java
+++ app/src/main/java/com/example/jeewan/menuIeams/send_feedback.java
@@ -1,49 +1,73 @@
 package com.example.jeewan.menuIeams;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.jeewan.MainScreenActivity;
 import com.example.jeewan.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class send_feedback extends AppCompatActivity {
+public class Send_Feedback extends AppCompatActivity {
     RatingBar ratingBar;
     EditText feedback;
     Button submit;
     Float ratedValue;
-
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-    feedbackform feedbackform;
+    Feedbackform feedbackform;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_feedback);
         ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingBar.setRating(1);
+        ratedValue=ratingBar.getRating();
         feedback = (EditText)findViewById(R.id.feedback);
-        database = FirebaseDatabase.getInstance();
-        feedbackform = new feedbackform();
-        myRef = database.getReference("feedbackform");
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
                 ratedValue = ratingBar.getRating();
             }
         });
+
         submit = (Button)findViewById(R.id.submitButton);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                feedbackform.setRating(ratedValue.toString());
-                feedbackform.setMessage(feedback.getText().toString().trim());
-                myRef.push().setValue(feedbackform);
-                //Toast.makeText(send_feedback.this, "Feedback received", Toast.LENGTH_SHORT).show();
+                submit.setEnabled(false);
+                if(feedback.getText().toString().isEmpty())
+                {
+                    submit.setEnabled(true);
+                    feedback.setError("Enter your feedback");
+                    return;
+                }
+
+                //add feedback in in firestore
+                feedbackform=new Feedbackform(ratedValue.toString(),feedback.getText().toString().trim());
+                FirebaseFirestore.getInstance().collection("Feedback").
+                        document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(feedbackform)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                startActivity(new Intent(getApplicationContext(),FeedbackSubmitted.class));
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                submit.setEnabled(true);
+                                Toast.makeText(getApplicationContext(),"Something went wrong,Please try again later!",Toast.LENGTH_LONG).show();
+                            }
+                        });
             }
         });
     }
